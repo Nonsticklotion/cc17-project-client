1e0a60db9fe94cca6d85e58c4e79f8a78cd6e047
Nonsticklotion <pisit676@gmail.com>
2024-09-05 13:07:21 +0700

    change card in  all product ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
 export default function Card({ product }) {
   return (
-    <div className="border-2 p-3 rounded-xl flex flex-col max-w-96 gap-4">
+    <div className="card card-compact bg-base-100 h-96 p-4 w-64 shadow-xl overflow-hidden">
+    <figure>
       <img
-        src={product?.bookPic}
-        alt=""
-        className="h-56 w-56 object-cover"
-      />
-      <div className="flex justify-between">
-        <div className="font-bold">{product?.bookName || "Empty"}</div>
-        <div className="font-bold">Price : ${product?.price || "Empty"}</div>
+       src={product?.bookPic}
+       alt=""
+       className="h-56 w-56 object-cover" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title overflow-hidden">{product?.bookName}</h2>
+      <div className="card-actions justify-end">
+        <button className="btn btn-neutral">{product?.price || "Empty"} Baht</button>
       </div>
     </div>
+  </div>
   );
 }
