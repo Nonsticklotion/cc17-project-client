e073c0ae682eb18c94a3e53d0a495aefe4fa76cc
Nonsticklotion <pisit676@gmail.com>
2024-09-05 12:06:04 +0700

    add corousel

---
+++ src/components/Slide.jsx
@@ -0,0 +1,88 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+
+export default function Slide({ slides, width = "full" }) {
+  const sliderRef = useRef(null);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    customPaging: () => <div className="w-3 h-3 bg-white rounded-full"></div>,
+    appendDots: (dots) => (
+      <div
+        style={{
+          backgroundColor: "transparent",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul style={{ margin: "0px" }}>{dots}</ul>
+      </div>
+    ),
+  };
+
+  const nextSlide = () => sliderRef.current.slickNext();
+  const previousSlide = () => sliderRef.current.slickPrev();
+
+  return (
+    <div className={`w-${width} h-96`}>
+      {" "}
+      {/* Locked height to 96 (24rem) */}
+      <div className="w-full h-full overflow-hidden">
+        <Slider ref={sliderRef} {...settings}>
+          {slides.map((slide, index) => (
+            <div key={index}>
+              <div className="flex flex-row p-3 h-full items-center justify-center focus:outline-none">
+                <div className="w-2/5 flex justify-center items-center">
+                  <img
+                    src={slide.bookPic}
+                    alt={slide.bookName}
+                    className="w-60 mx-auto"
+                    loading="lazy"
+                  />
+                </div>
+                <div className="w-3/5 pl-8">
+                  <h2 className="text-3xl font-bold mb-4">{slide.bookName}</h2>
+                  <h3 className="text-2xl mb-2">{slide.description}</h3>
+                  <p className="text-xl text-gray-800 mb-2">{slide.author}</p>
+                  <p className="text-xl text-red-500 mb-4">฿{slide.price}</p>
+                  <Link
+                    to={`/books/${slide.id}`}
+                    className="bg-pink-500 text-white px-4 py-2 rounded-full"
+                  >
+                    Go to Book
+                  </Link>
+                </div>
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                  <button
+                    onClick={previousSlide}
+                    className="btn btn-circle bg-gray-800 text-white hover:bg-gray-700"
+                    aria-label="Previous Slide"
+                  >
+                    ❮
+                  </button>
+                  <button
+                    onClick={nextSlide}
+                    className="btn btn-circle bg-gray-800 text-white hover:bg-gray-700"
+                    aria-label="Next Slide"
+                  >
+                    ❯
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,75 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import adminApi from "../api/admin";
+import { toast } from "react-toastify";
+import Slide from "../components/Slide";
+
+const mockData = [
+  {
+    imgSrc:
+      "https://lh5.googleusercontent.com/d/1FziDYPgkrSjdYLgTZy7Z_oN9PJX8iAWc",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 1",
+    link: {
+      linkSrc: "/hospital/1",
+      button: "line: โรงพยาบาล 1",
+    },
+  },
+  {
+    imgSrc:
+      "https://lh5.googleusercontent.com/d/18BT_zH7deLikFY2O-lj8fB1P1y982tre",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 2",
+    link: {
+      linkSrc: "/hospital/2",
+      button: "line: โรงพยาบาล 2",
+    },
+  },
+  {
+    imgSrc:
+      "https://lh5.googleusercontent.com/d/1-_VfWeaPLJm33Bs-zg4I2ZYPnH8fF7F1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 3",
+    link: {
+      linkSrc: "/hospital/3",
+      button: "line: โรงพยาบาล 3",
+    },
+  },
+  {
+    imgSrc:
+      "https://lh5.googleusercontent.com/d/1Wg7K4Su8wdVeB8vCXUgOm3_-8DsO9zzw",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 4",
+    link: {
+      linkSrc: "/hospital/4",
+      button: "line: โรงพยาบาล 4",
+    },
+  },
+  {
+    imgSrc:
+      "https://lh5.googleusercontent.com/d/1DenZx63654QN88_j6Tj5QuMkdpdQlo5H",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 5",
+    link: {
+      linkSrc: "/hospital/5",
+      button: "line: โรงพยาบาล 5",
+    },
+  },
+];
 
 export default function HomePage() {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await adminApi.getProduct();
+        setProduct(response.data.data);
+        console.log(response);
+      } catch (err) {
+        toast.error("Can't fetch data");
+      }
+    };
+    fetchProducts();
+  }, []);
   return (
     <div className="min-h-screen flex flex-col pt-5">
-      <section className=" h-96 flex justify-between items-center bg-gray-100 p-8 mb-5 rounded-lg shadow-md ">
+      {/* <section className=" h-96 flex justify-between items-center bg-gray-100 p-8 mb-5 rounded-lg shadow-md ">
         <div className="w-1/2 pl-5">
           <h2 className="text-4xl font-bold mb-4">Ipsum Dolor Si</h2>
           <p className="text-gray-700 mb-4">
@@ -18,6 +84,7 @@ export default function HomePage() {
             Read More
           </Link>
         </div>
+
         <div className="w-1/2 pl-12 flex justify-center items-center">
           <img
             src="https://picsum.photos/200/300?grayscale"
@@ -25,6 +92,9 @@ export default function HomePage() {
             className="w-60 mx-auto"
           />
         </div>
+      </section> */}
+      <section className="h-96 flex justify-between items-center bg-pink-100 p-8 mb-5 rounded-lg shadow-md ">
+        <Slide slides={product} width="full" fit="object-contain" />
       </section>
 
       <section className="h-96 flex justify-between items-center bg-pink-100 p-8 mb-5 rounded-lg shadow-md ">
